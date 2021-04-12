c477ea3a2f3c9c153fff7f1265efa9c8c427deb2
austinjdowney <austinjdowney@gmail.com>
2021-04-12 14:11:34 -0600

    clock position abs attempt

--- app/Models/Todo.js
+++ app/Models/Todo.js
@@ -1,20 +1,20 @@
 export default class Todo {
-    constructor(data) {
-        this.description = data.description
-        this.id = data.id
-        this.completed = data.completed
+  constructor(data) {
+    this.description = data.description
+    this.id = data.id
+    this.completed = data.completed
 
-    }
+  }
 
-    get Template() {
+  get Template() {
 
-        //let todos = ProxyState.todos.filter(t => t.Id === this.id)
-        //let total = todos.length
-        //let completed = todos.filter(t => t.completed).length
+    //let todos = ProxyState.todos.filter(t => t.Id === this.id)
+    //let total = todos.length
+    //let completed = todos.filter(t => t.completed).length
 
 
-        if (this.completed) {
-            return /*html*/ `
+    if (this.completed) {
+      return /*html*/ `
   <div class="row  mb-2 bg-dark text-light">
     <div class="col-2">
       <button onclick="app.todosController.completedTodo('${this.id}')" class="btn btn-success"><i class="fas fa-check-square"></i></button>
@@ -27,8 +27,8 @@ export default class Todo {
     </div>
 </div>
     `
-        } else {
-            return /*html*/ `
+    } else {
+      return /*html*/ `
   <div class="row mb-2 bg-dark text-light">
     <div class="col-2">
       <button onclick="app.todosController.completedTodo('${this.id}')" class="btn btn-success"><i class="fas fa-check-square"></i></button>
@@ -41,14 +41,14 @@ export default class Todo {
     </div>
 </div>
     `
-        }
     }
+  }
 
 
 
-    //get Todos() {
-    //    let todo = ProxyState.todos.filter(t => t.todoId === this.id)
-    //    let template = ''
-    //    todo.forEach(t => template += t.Template)
-    //    return template
+  //get Todos() {
+  //    let todo = ProxyState.todos.filter(t => t.todoId === this.id)
+  //    let template = ''
+  //    todo.forEach(t => template += t.Template)
+  //    return template
 }
